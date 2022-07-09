64074d8cb9249cd1a5489a4338d8c2fd21b408ed
“Corinne <corinne.canavan@gmail.com>
2022-07-09 12:58:03 -0600

    Adjust styling for movie poster and movie info sections within MovieDisplay

--- src/MovieDisplay.js
+++ src/MovieDisplay.js
@@ -8,13 +8,13 @@ const MovieDisplay = () => {
   // }
 
   return (
-    <div style={{backgroundImage: `url("https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg")`}}>
-      <div>
+    <div className='movieBackdrop' style={{backgroundImage: `url("https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg")`}}>
+      <div className='movieContainer'>
         <img className='moviePoster' src="https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg" />
-      </div>
-      <div>
-        <p>Movie Info</p>
-        <img className='arrow' src='' />
+        <div className='movieInfo'>
+          <p>Movie Info</p>
+          <img className='arrow' src='' />
+        </div>
       </div>
     </div>
   )
