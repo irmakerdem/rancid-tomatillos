008fdd6436c457b0709dd7e6dcc8010d52ad3343
“Corinne <corinne.canavan@gmail.com>
2022-07-14 17:11:08 -0600

    Remove code comments

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -8,50 +8,20 @@ class SearchBar extends Component {
       movieTitle: ''
     }
   }
-
-  // const displayUniqueDestinations = (uniqueDestinations) => {
-  //   formDestinations.innerHTML = `<option value="choose">Choose A Destination:</option>`;
-  //   uniqueDestinations.forEach((dest) => {
-  //     formDestinations.innerHTML += `<option value="${dest}">${dest}</option>`
-  //   })
-  // }
-
-
+  
   render() {
-    // console.log("hereeeee", this.props.movies)
-    // console.log("hereeeee2", this.props)
-    // this.props.movies is an array of objects
-    // const movieTitles = this.props.movies.sort((a,b) => {
-    //   console.log("aaaaa", a.title)
-    //   return a.title.localeCompare(b.title)
-    // }).map(movie => {
-      // console.log("movieeee", movie)
-    //   return (
-    //     <option value={movie.title} key={movie.id}>{movie.title}</option>
-    //   )
-    // })
-
     const movieTitles = [...this.props.movies].sort((a,b) => a.title.localeCompare(b.title))
         .map(movie => {
           return (
           <option value={movie.title} key={movie.id}>{movie.title}</option>
           )
         })
-    
-    // this.props.movies
-    // .sort((a,b) => a.title.localeCompare(b.title))
-    // .map(movie => {
-    //   return (
-    //   <option value={movie.title} key={movie.id}>{movie.title}</option>
-    //   )
-    // })
 
     return (
       <form>
         <select>
           <option value="choose">Choose A Movie:</option>
           {movieTitles}
-          {/* <option value="fightClub">Fight Club</option> */}
         </select>
       </form>
     )
