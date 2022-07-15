9adc2d773c79ad4a0480c38708cdaa377dcc96ae
“Corinne <corinne.canavan@gmail.com>
2022-07-14 19:47:08 -0600

    Update state in SearchBar, add Link to button to switch to MovieDisplay page once movie title has been selected

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,28 +1,53 @@
 import React, {Component} from 'react';
 import './SearchBar.css'
+import { Link } from 'react-router-dom';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movieTitle: ''
+      title: '',
+      id: ''
     }
   }
-  
+
+  // user selects Title from movie dropdown
+  // hits "Go" button 
+        // need to add button 
+  // go to Movie Display page - new URL
+        
+
+  handleChange = (event) => {
+    // const { id, title } = event.target.value
+    console.log("event", event);
+    // console.log("id", id)
+    this.setState({ 
+      id: event.target.value
+      // title: title
+      // id: event.target.id
+    }
+      
+    )
+  }
+
   render() {
     const movieTitles = [...this.props.movies].sort((a,b) => a.title.localeCompare(b.title))
         .map(movie => {
+          // console.log("movie", movie);
           return (
-          <option value={movie.title} key={movie.id}>{movie.title}</option>
+          <option value={movie.id} key={movie.id}>{movie.title}</option>
           )
         })
 
     return (
       <form>
-        <select>
+        <select onChange={(event) => this.handleChange(event)}>
           <option value="choose">Choose A Movie:</option>
           {movieTitles}
         </select>
+        <Link to={`/${this.state.id}`}>
+          <button>GO!</button>
+        </ Link>
       </form>
     )
   }
