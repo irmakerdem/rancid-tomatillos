4fd489fcb162a0cf106cd5e7702aec615fb59932
“Corinne <corinne.canavan@gmail.com>
2022-07-17 14:14:21 -0600

    Adjust spacing, add hover effect to GO button for Dropdown

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Card.css';
 import { Link } from 'react-router-dom';
 
-const Card = ({title, img, rating, id}) => {
+const Card = ({ title, img, rating, id }) => {
   return (
     <div className='card'>
       <Link to={`/${id}`}>
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/Dropdown.css';
 import { Link } from 'react-router-dom';
 
