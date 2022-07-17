a32bd75faa08bd1cc3253b09615307893d837b65
“Corinne <corinne.canavan@gmail.com>
2022-07-17 15:00:21 -0600

    Add opacity to movie backdrop image, adjust movieInfo box sizing, test conditional for movies without tagline

--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -9,15 +9,13 @@ class MovieDisplay extends Component {
     super(props);
     this.state = {
       movie: {},
-      error: '',
-      isLoading: false
+      error: ''
     }
   }
 
   componentDidMount = () => {
-    this.setState({ isLoading: true })
     getMovieDetails(this.props.id)
-      .then(data => this.setState({ movie: data.movie, isLoading: false }))
+      .then(data => this.setState({ movie: data.movie }))
       .catch(error => {
         this.setState({ error: "ERROR: " + error.message })
       })
@@ -26,22 +24,22 @@ class MovieDisplay extends Component {
   render() {
     return (
       <>
-        { this.state.error && <p>{this.state.error}</p> }
+        {this.state.error && <p className='error'>{this.state.error}</p>}
         <div className='movieBackdrop' style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}}>
           <div className='movieContainer'>
-            <img className='moviePoster' src={this.state.movie.poster_path} alt={`${this.state.movie.title} poster`} style={{opacity: '1'}}/>
+            <img className='moviePoster' src={this.state.movie.poster_path} alt={`${this.state.movie.title} poster`}/>
             <section className='movieInfo'>
               <p className='infoTitle'>Title: <span className='infoContent'> {this.state.movie.title}</span></p>
               <p className='infoTitle'>Average Rating: <span className='infoContent'>{Number(this.state.movie.average_rating).toFixed(1)}</span></p>
               <p className='infoTitle'>Overview: <span className='infoContent'>{this.state.movie.overview}</span></p>
               <p className='infoTitle'>Release Date: <span className='infoContent'>{this.state.movie.release_date}</span></p>
               <p className='infoTitle'>Runtime: <span className='infoContent'>{this.state.movie.runtime} minutes</span></p>
-              <p className='infoTitle'>Tagline: <span className='infoContent'>{this.state.movie.tagline}</span></p>
-              <p className='infoTitle'>Genres: <span className='infoContent'>{[this.state.movie.genres].join()}</span></p>
-              <div className="arrow-flex">
+              <p className='infoTitle'>Tagline: <span className='infoContent'>{this.state.movie.tagline ? this.state.movie.tagline : 'N/A'}</span></p>
+              <p className='infoTitle'>Genres: <span className='infoContent'>{[this.state.movie.genres].join(', ')}</span></p>
+              <div className='arrow-flex'>
                 <Link to="/">
-                  <input type="image" className='arrow' src={arrowIcon} alt='arrow icon'/>
-                </Link>
+                  <input type='image' className='arrow' src={arrowIcon} alt='arrow icon'/>
+                </ Link>
               </div>
             </section>
           </div>
